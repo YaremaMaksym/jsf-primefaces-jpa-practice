046baf1f055c5186e92da481c8845c9972f2436e
Maksym Yarema <macsym2@gmail.com>
2023-11-17 09:34:47 +0200

    working with db using jdbc

---
+++ src/main/java/com/example/jsfprimefacesjpapractice/beans/ArtistBean.java
@@ -0,0 +1,36 @@
+package com.example.jsfprimefacesjpapractice.beans;
+
+import com.example.jsfprimefacesjpapractice.dao.ArtistDao;
+import com.example.jsfprimefacesjpapractice.data.Artist;
+import jakarta.ejb.EJB;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@SessionScoped
+public class ArtistBean implements Serializable {
+
+    @EJB
+    private ArtistDao artistDao;
+
+    @Getter @Setter
+    private Artist artist = new Artist();
+
+    public List<Artist> getArtistList() {
+        return artistDao.findAll();
+    }
+
+    public void add() {
+        artistDao.add(artist);
+        artist = new Artist();
+    }
+
+    public void delete(int id) {
+        artistDao.delete(id);
+    }
+}
---
+++ src/main/java/com/example/jsfprimefacesjpapractice/data/Artist.java
@@ -0,0 +1,13 @@
+package com.example.jsfprimefacesjpapractice.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Artist {
+    private int id;
+    private String name;
+}
